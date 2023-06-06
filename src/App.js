33f6ccde81a2d9f40bdbed781a273af693a3a176
Jishnu Choyi <c.jishnu@gmail.com>
2023-06-06 22:36:55 +0530

    install amplitude

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.scss';
 import { ThemeProvider, createTheme } from "@mui/material";
-import CTACardList from "./components/CTACardList";
 import Header from "./components/Header";
 import Landing from './pages/Landing';
+import * as amplitude from '@amplitude/analytics-browser';
 
 // Or Create your Own theme:
 const iconTheme = createTheme({
@@ -16,9 +16,11 @@ const iconTheme = createTheme({
   }
 });
 
-function App() {
-  return (
+amplitude.init('71a74323057e3cdb289336830873113f', undefined, { defaultTracking: { sessions: true, pageViews: true, formInteractions: true, fileDownloads: true } });
 
+function App() {
+  amplitude.track('Page view');
+  return (
     <ThemeProvider theme={iconTheme}>
       <Header />
       <div className="container">
