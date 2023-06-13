c5971636e5a8be2e6efbf28670a55e79c22d07cb
Jishnu Choyi <c.jishnu@gmail.com>
2023-06-13 15:39:18 +0530

    make ctaCard component obsolete

--- src/components/CTACardList/index.js
+++ src/components/CTACardList/index.js
@@ -1,43 +1,63 @@
-import styles from './ctaCardList.module.scss';
-import CTACard from "../CTACard";
+import styles from "./ctaCardList.module.scss";
+import cardStyles from "./ctaCard.module.scss";
 
 function CTACardList() {
     const cardData = [
         {
             title: "Work experience",
-            subTitle: "Almost everything that I have ever worked on professionally",
+            subTitle:
+                "Almost everything that I have ever worked on professionally",
             styles: {
-                backgroundColor: "#FFFFFF"
+                backgroundColor: "#FFFFFF",
             },
-            isTop: true
+            isTop: true,
         },
         {
             title: "Creative projects",
             subTitle: "Those side projects that make you feel alive",
             styles: {
-                backgroundColor: "#EBDED4"
-            }
+                backgroundColor: "#EBDED4",
+            },
         },
         {
             title: "Education",
             subTitle: "Campuses and friends that made me who I am",
             styles: {
                 backgroundColor: "#C47F38",
-                color: "#FFFFFF"
-            }
-        }
+                color: "#FFFFFF",
+            },
+        },
     ];
     return (
         <div className={styles["container"]}>
-            {
-                cardData.map((item, index) => {
-                    return <CTACard key={index} id={styles['cta-card-' + index]}
-                        title={item.title} subTitle={item.subTitle}
-                        styles={{ ...item.styles, zIndex: cardData.length - index }}
-                    />
-
-                })
-            }
+            {cardData.map((item, index) => {
+                return (
+                    <div
+                        key={index}
+                        id={styles["cta-card-" + index]}
+                        className={cardStyles["container"]}
+                        style={{
+                            ...item.styles,
+                            zIndex: cardData.length - index,
+                        }}
+                    >
+                        <div
+                            className={
+                                cardStyles["content"] +
+                                " " +
+                                styles["cta-card-content-" + index]
+                            }
+                        >
+                            <div className={cardStyles["left"]}>
+                                {item.title}
+                            </div>
+                            <div className={cardStyles["right"]}>
+                                {item.subTitle}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 }
